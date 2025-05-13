e80ca116ec360792e173471ede167795db26e690
anmolpreet.singh.7e8 <anmolpreet.singh.7e8@itb.cat>
2025-05-13 14:01:23 +0200

    splash

--- app/src/main/java/com/example/mapsapp/MainActivity.kt
+++ app/src/main/java/com/example/mapsapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.mapsapp.ui.navigation.MainNavigationWrapper
 import com.example.mapsapp.ui.theme.MapsAppTheme
 
@@ -13,6 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             MapsAppTheme {
                 MainNavigationWrapper()
